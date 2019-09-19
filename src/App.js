3d24a04dcb18e56c6ed6a938482df98daed2cb96
Gabukas <gabukas@new-host-2.broadband>
2019-09-19 20:36:33 +0100

    Added React, CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo192.png';
+import address from './placeholder.png'
+import phone from './phone-call.png'
+import email from './envelope.png'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card-inner">
+        <div className="front">
+          <div className="title">
+            <h1>Name Surname</h1>
+            <p>Official position</p>
+          </div>
+          <div className="divider"></div>
+          <div className="contact">
+            <div className="icon-text">
+              <img src={phone} alt="phone"/>
+              <p>+111 22 333</p>
+            </div>
+            <div className="icon-text">
+              <img src={address} alt="address"/>
+              <p>123 Your Street</p>
+            </div>
+            <div className="icon-text">
+              <img src={email} alt="email"/>
+              <p>youremail@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="back">
+          <div className="card-logo">
+            <img src={logo} alt="logo"/>
+            <p className="company-name">Company Name</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
